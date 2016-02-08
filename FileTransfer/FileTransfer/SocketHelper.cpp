c894cdb91e05888b5b354818d6e5f5265275bf25
jerrybeanman <jerry1bean@hotmail.com>
2016-02-08 04:07:25 -0800

    Commented SocketHelper.cpp and SocketHelper.h

--- FileTransfer/FileTransfer/SocketHelper.cpp
+++ FileTransfer/FileTransfer/SocketHelper.cpp
@@ -1,8 +1,54 @@
+/*------------------------------------------------------------------------------------------------------------------
+-- SOURCE FILE:	SocketHelper.c - Provides wrapper functions to communicate with the socket
+--
+-- PROGRAM:		Protocol Analyzer
+--
+-- FUNCTIONS:	BOOL S_TCPSend(LPSOCKET_INFORMATION SOCKET_INFO)
+--				BOOL S_UDPSend(LPSOCKET_INFORMATION SOCKET_INFO, SOCKADDR_IN * Server)
+--				BOOL S_TCPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK)
+--				BOOL S_UDPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK, DWORD * RecvBytes)
+--				BOOL S_TCPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
+--				BOOL S_UDPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
+--				void UpdateTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO, DWORD BytesRecieved, LPSOCKET_INFORMATION SOCKET_INFO)
+--				void PrintTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO)
+--				long delay(SYSTEMTIME t1, SYSTEMTIME t2)
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- NOTES: Provides wrapper function for TCP and UDP services to communicate with a socket. Such as sending, recieving,
+--			and connecting. 
+--			Also provide functions to calculate statistics for a tramission 
+--------------------------------------------------------------------------------------------------------------------*/
 #include "SocketHelper.h"
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_TCPSend
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_TCPSend(LPSOCKET_INFORMATION SOCKET_INFO)
+--					LPSOCKET_INFORMATION SOCKET_INFO : Socket information structure for writing
+--
+-- RETURNS: success value 
+--
+-- NOTES: Writes to a socket for TCP service 
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_TCPSend(LPSOCKET_INFORMATION SOCKET_INFO)
 {
 	/* Post a message back to the socket for aknowledgement */
-	if (WSASend(SOCKET_INFO->Socket,	/* Accepted socket						*/
+	if (WSASend(SOCKET_INFO->Socket,	/* Writing socket						*/
 		&(SOCKET_INFO->DataBuf),		/* Pointer to WSABUF					*/
 		1,								/* Only 1 WSABUF						*/
 		&SOCKET_INFO->BytesSEND,		/* Bytes that are sent					*/
@@ -19,14 +65,32 @@ BOOL S_TCPSend(LPSOCKET_INFORMATION SOCKET_INFO)
 	return TRUE;
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_UDPSend
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_TCPSend(LPSOCKET_INFORMATION SOCKET_INFO)
+--					LPSOCKET_INFORMATION SOCKET_INFO : Socket information structure for writing
+--
+-- RETURNS: success value
+--
+-- NOTES: Writes to a socket, and the target host destination for UDP service 
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_UDPSend(LPSOCKET_INFORMATION SOCKET_INFO, SOCKADDR_IN * Server)
 {
-	if (sendto(SOCKET_INFO->Socket,	/* Accepted socket						*/
-		(SOCKET_INFO->DataBuf.buf),		/* Pointer to WSABUF					*/
-		SOCKET_INFO->DataBuf.len,				/* Only 1 WSABUF						*/
-		0,							/* Bytes that are sent					*/
-		(SOCKADDR *)Server,
-		sizeof(*Server))			/* No completion routine				*/
+	if (sendto(SOCKET_INFO->Socket,		/* Writing socket						*/
+		(SOCKET_INFO->DataBuf.buf),		/* Message content						*/
+		SOCKET_INFO->DataBuf.len,		/* Size of the message					*/
+		0,								/* Bytes that are sent					*/
+		(SOCKADDR *)Server,				/* Server socket address structure		*/
+		sizeof(*Server))				/* size of the socket address structure	*/
 		< 0)
 	{
 		if (WSAGetLastError() != WSA_IO_PENDING)
@@ -37,6 +101,25 @@ BOOL S_UDPSend(LPSOCKET_INFORMATION SOCKET_INFO, SOCKADDR_IN * Server)
 	return TRUE;
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_TCPRecieve
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_TCPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK)
+--					LPSOCKET_INFORMATION SOCKET_INFO : Socket information structure to recive packets from
+--					BOOL				 isCALLBACK  : Do we want completion routine or nah?
+--
+-- RETURNS: success value
+--
+-- NOTES: Recives from a socket for TCP service, also calls the completion routine function if needed 
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_TCPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK)
 {
 	DWORD RecvBytes;
@@ -58,6 +141,26 @@ BOOL S_TCPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK)
 	return TRUE;
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_UDPRecieve
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_UDPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK, DWORD * RecvBytes)
+--					LPSOCKET_INFORMATION SOCKET_INFO : Socket information structure to recive packets from
+--					BOOL				 isCALLBACK  : Do we want completion routine or nah?
+--					DWORD			*	 RecvBytes   : Bytes recived 
+--
+-- RETURNS: success value
+--
+-- NOTES: Recives from a socket for TCP service, also calls the completion routine function if needed
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_UDPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK, DWORD * RecvBytes)
 {
 
@@ -66,11 +169,11 @@ BOOL S_UDPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK, DWORD * Rec
 	int client_len = sizeof(client);
 
 	if ((*RecvBytes = recvfrom(SOCKET_INFO->Socket,		/* Accepted socket					*/
-		(SOCKET_INFO->Buffer),							/* Pointer to WSABUF				*/
-		DATA_BUFSIZE,									/* Only one WSABUF					*/
-		Flags,											/* Total bytes that is recieved		*/
-		(SOCKADDR *)&client,
-		&client_len)) 
+		(SOCKET_INFO->Buffer),							/* Message buffer to recieve		*/
+		DATA_BUFSIZE,									/* Maximum data to recieve			*/
+		Flags,											/* No modification					*/
+		(SOCKADDR *)&client,							/* Server socket address structure		*/
+		&client_len))									/* size of the socket address structure	*/
 		< 0)
 	{
 		return FALSE;
@@ -78,6 +181,25 @@ BOOL S_UDPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK, DWORD * Rec
 	return TRUE;
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_TCPConnect
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_TCPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
+--					SOCKET			WritingSocket : Socket to connect to
+--					SOCKADDR_IN	*	InternetAddr  : Address structure that stores the target address 
+--
+-- RETURNS: success value
+--
+-- NOTES: Attempt to connect to a server
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_TCPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
 {
 
@@ -95,8 +217,30 @@ BOOL S_TCPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
 	return TRUE;
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_UDPConnect
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_UDPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
+--					SOCKET			WritingSocket : Socket to connect to
+--					SOCKADDR_IN	*	InternetAddr  : Address structure that stores the client's address
+--
+-- RETURNS: success value
+--
+-- NOTES: Not actually "connecting" to a server since its UDP. It basically initializes the client's address structure
+--			and binds it to the socket 
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_UDPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
 {
+
+	/* Initalize client address for UDP */
 	InternetAddr->sin_family = AF_INET;
 	InternetAddr->sin_port = htons(0);
 	InternetAddr->sin_addr.s_addr = htonl(INADDR_ANY);
@@ -112,12 +256,38 @@ BOOL S_UDPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
 	return TRUE;
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	UpdateTransmission
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	void UpdateTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO, DWORD BytesRecieved, LPSOCKET_INFORMATION SOCKET_INFO)
+--					LPTRANSMISSION_INFORMATION TRANS_INFO : Contain statistics on current transmission 
+--					DWORD BytesRecieved : Total bytes recieved from S_UDPRecv or S_TCPRecv
+--					LPSOCKETINFORMATION SOCKET_INFO : Socket information structure 
+--
+-- RETURNS: void
+--
+-- NOTES: Updates the transmission information structture for statistics 
+--------------------------------------------------------------------------------------------------------------------*/
 void UpdateTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO, DWORD BytesRecieved, LPSOCKET_INFORMATION SOCKET_INFO)
 {
+	/* If a reciev call failed */
 	if (BytesRecieved == UINT_MAX)
 		return;
+
+	/* Append BytresRECV value */
 	SOCKET_INFO->BytesRECV += BytesRecieved;
+
+	/* Check if we have enough bytes to fill up a packet */
 	int Result = (int)(SOCKET_INFO->BytesRECV / TRANS_INFO->PacketSize);
+
 	sprintf(StrBuff, "Data recieved! Size: %d\n", BytesRecieved);
 	AppendToStatus(hStatus, StrBuff);
 
@@ -128,10 +298,29 @@ void UpdateTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO, DWORD BytesReciev
 		SOCKET_INFO->BytesRECV -= Result	/* Add remaining bytes to SOCKET_INFORMATION		*/		
 			* TRANS_INFO->PacketSize;
 	}
+
+	/* Write the packet content to a output file */
 	fprintf(fp, SOCKET_INFO->DataBuf.buf);
-	OutputDebugString(SOCKET_INFO->DataBuf.buf);
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	PrintTransmission
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	void PrintTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO)
+--					LPTRANSMISSION_INFORMATION TRANS_INFO : Transmission information to print out
+--
+-- RETURNS: void
+--
+-- NOTES: Prints the transmission information values 
+--------------------------------------------------------------------------------------------------------------------*/
 void PrintTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO)
 {
 	AppendToStatus(hStatus, "**************************************************\n");
@@ -150,7 +339,25 @@ void PrintTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO)
 	AppendToStatus(hStatus, "**************************************************\n");
 }
 
-// Compute the delay between tl and t2 in milliseconds
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	delay
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	long delay(SYSTEMTIME t1, SYSTEMTIME t2)
+--					SYSTEMTIME t1 : Starting time
+--					SYSTEMTIME t2 : Ending Time 
+--
+-- RETURNS: void
+--
+-- NOTES: Compute the delay between tl and t2 in milliseconds
+--------------------------------------------------------------------------------------------------------------------*/
 long delay(SYSTEMTIME t1, SYSTEMTIME t2)
 {
 	long d;
--- FileTransfer/FileTransfer/SocketHelper.h
+++ FileTransfer/FileTransfer/SocketHelper.h
@@ -1,12 +1,218 @@
+/*------------------------------------------------------------------------------------------------------------------
+-- SOURCE FILE:	SocketHelper.c - Provides wrapper functions to communicate with the socket
+--
+-- PROGRAM:		Protocol Analyzer
+--
+-- FUNCTIONS:	BOOL S_TCPSend(LPSOCKET_INFORMATION SOCKET_INFO)
+--				BOOL S_UDPSend(LPSOCKET_INFORMATION SOCKET_INFO, SOCKADDR_IN * Server)
+--				BOOL S_TCPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK)
+--				BOOL S_UDPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK, DWORD * RecvBytes)
+--				BOOL S_TCPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
+--				BOOL S_UDPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
+--				void UpdateTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO, DWORD BytesRecieved, LPSOCKET_INFORMATION SOCKET_INFO)
+--				void PrintTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO)
+--				long delay(SYSTEMTIME t1, SYSTEMTIME t2)
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- NOTES: Provides wrapper function for TCP and UDP services to communicate with a socket. Such as sending, recieving,
+--			and connecting.
+--			Also provide functions to calculate statistics for a tramission
+--------------------------------------------------------------------------------------------------------------------*/
 #pragma once
 #include "Global.h"
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_TCPSend
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_TCPSend(LPSOCKET_INFORMATION SOCKET_INFO)
+--					LPSOCKET_INFORMATION SOCKET_INFO : Socket information structure for writing
+--
+-- RETURNS: success value
+--
+-- NOTES: Writes to a socket for TCP service
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_TCPSend(LPSOCKET_INFORMATION SOCKET_INFO);
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_UDPSend
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_TCPSend(LPSOCKET_INFORMATION SOCKET_INFO)
+--					LPSOCKET_INFORMATION SOCKET_INFO : Socket information structure for writing
+--
+-- RETURNS: success value
+--
+-- NOTES: Writes to a socket, and the target host destination for UDP service
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_UDPSend(LPSOCKET_INFORMATION SOCKET_INFO, SOCKADDR_IN * Server);
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_TCPRecieve
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_TCPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK)
+--					LPSOCKET_INFORMATION SOCKET_INFO : Socket information structure to recive packets from
+--					BOOL				 isCALLBACK  : Do we want completion routine or nah?
+--
+-- RETURNS: success value
+--
+-- NOTES: Recives from a socket for TCP service, also calls the completion routine function if needed
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_TCPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK);
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_UDPRecieve
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_UDPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK, DWORD * RecvBytes)
+--					LPSOCKET_INFORMATION SOCKET_INFO : Socket information structure to recive packets from
+--					BOOL				 isCALLBACK  : Do we want completion routine or nah?
+--					DWORD			*	 RecvBytes   : Bytes recived
+--
+-- RETURNS: success value
+--
+-- NOTES: Recives from a socket for TCP service, also calls the completion routine function if needed
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_UDPRecieve(LPSOCKET_INFORMATION SOCKET_INFO, BOOL isCALLBACK, DWORD * RecvBytes);
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_TCPConnect
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_TCPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
+--					SOCKET			WritingSocket : Socket to connect to
+--					SOCKADDR_IN	*	InternetAddr  : Address structure that stores the target address
+--
+-- RETURNS: success value
+--
+-- NOTES: Attempt to connect to a server
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_TCPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr);
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	S_UDPConnect
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	BOOL S_UDPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr)
+--					SOCKET			WritingSocket : Socket to connect to
+--					SOCKADDR_IN	*	InternetAddr  : Address structure that stores the client's address
+--
+-- RETURNS: success value
+--
+-- NOTES: Not actually "connecting" to a server since its UDP. It basically initializes the client's address structure
+--			and binds it to the socket
+--------------------------------------------------------------------------------------------------------------------*/
 BOOL S_UDPConnect(SOCKET WritingSocket, SOCKADDR_IN * InternetAddr);
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	UpdateTransmission
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	void UpdateTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO, DWORD BytesRecieved, LPSOCKET_INFORMATION SOCKET_INFO)
+--					LPTRANSMISSION_INFORMATION TRANS_INFO : Contain statistics on current transmission
+--					DWORD BytesRecieved : Total bytes recieved from S_UDPRecv or S_TCPRecv
+--					LPSOCKETINFORMATION SOCKET_INFO : Socket information structure
+--
+-- RETURNS: void
+--
+-- NOTES: Updates the transmission information structture for statistics
+--------------------------------------------------------------------------------------------------------------------*/
 void UpdateTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO, DWORD BytesRecieved, LPSOCKET_INFORMATION SOCKET_INFO);
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	PrintTransmission
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	void PrintTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO)
+--					LPTRANSMISSION_INFORMATION TRANS_INFO : Transmission information to print out
+--
+-- RETURNS: void
+--
+-- NOTES: Prints the transmission information values
+--------------------------------------------------------------------------------------------------------------------*/
 void PrintTransmission(LPTRANSMISSION_INFORMATION TRANS_INFO);
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	delay
+--
+-- DATE:		Febuary 6th, 2016
+--
+-- REVISIONS:
+--
+-- DESIGNER:	Ruoqi Jia
+--
+-- PROGRAMMER:	Ruoqi Jia
+--
+-- INTERFACE:	long delay(SYSTEMTIME t1, SYSTEMTIME t2)
+--					SYSTEMTIME t1 : Starting time
+--					SYSTEMTIME t2 : Ending Time
+--
+-- RETURNS: void
+--
+-- NOTES: Compute the delay between tl and t2 in milliseconds
+--------------------------------------------------------------------------------------------------------------------*/
 long delay(SYSTEMTIME t1, SYSTEMTIME t2);
